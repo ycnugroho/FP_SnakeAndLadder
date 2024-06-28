0f24390c0c6ad91bce1d3c0d23b5940dcbc7a5fb
ycnugroho <5026231032@student.its.ac.id>
2024-06-28 16:38:06 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+/**
+ * -----------------------------------------------------
+ * ES234211 - Programming Fundamental
+ * Genap - 2023/2024
+ * Group Capstone Project: Snake and Ladder Game
+ * -----------------------------------------------------
+ * Class    : C
+ * Group    : 9
+ * Members  :
+ * 1. 5026231032 - Yokanan Prawira Nugroho
+ * 2. 5026231087 - Shahnaz Ariqah Simanullang
+ * 3. 5026231190 - Antony Purnamarasid August Ratulangi
+ * ------------------------------------------------------
+ */
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        SnakeAndLadder g1 = new SnakeAndLadder(100);
+        g1.play();
+    }
+}
